278cb0a5a5a539c54f00989c6309b7f7b269e5fa
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-07-09 11:31:54 +0800

    Fix wrong condition checking

--- src/ForceSensorCalibration.cpp
+++ src/ForceSensorCalibration.cpp
@@ -3,7 +3,7 @@
 ForceSensorCalibration::ForceSensorCalibration(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::fsm::Controller(rm, dt, config)
 {
-  if(config.has(rm->name))
+  if(!config.has(rm->name))
   {
     mc_rtc::log::error_and_throw<std::runtime_error>("No configuration section for {}, add a configuration for this robot before attempting calibration", rm->name);
   }
