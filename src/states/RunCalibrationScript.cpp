550dd4ca2fca014a6ba365f9ed359ced6af4dc4e
Arnaud TANGUY <arn.tanguy@gmail.com>
2022-10-13 14:05:49 +0200

    [thread] Do not lower priority on windows systems

--- src/states/RunCalibrationScript.cpp
+++ src/states/RunCalibrationScript.cpp
@@ -73,15 +73,20 @@ void RunCalibrationScript::start(mc_control::fsm::Controller & ctl_)
     }
     completed_ = true;
   });
+#ifndef WIN32
   // Lower thread priority so that it has a lesser priority than the real time
   // thread
   auto th_handle = th_.native_handle();
   int policy = 0;
   sched_param param{};
   pthread_getschedparam(th_handle, &policy, &param);
-  mc_rtc::log::info("[{}] Calibration thread priority: {}", name(), param.sched_priority);
   param.sched_priority = 80;
-  pthread_setschedparam(th_handle, SCHED_RR, &param);
+  if(pthread_setschedparam(th_handle, SCHED_RR, &param) != 0)
+  {
+    mc_rtc::log::warning("[{}] Failed to lower calibration thread priority. If you are running on a real-time system, "
+                         "this might cause latency to the real-time loop.");
+  }
+#endif
 }
 
 bool RunCalibrationScript::run(mc_control::fsm::Controller &)
